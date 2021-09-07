a8a1403c4902ae9c4f037f0ede0424545f9d7d7c
Somnath Pawar <samlpawar@gmail.com>
2021-09-07 18:21:30 +0530

    Forced SSL URL:

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,8 @@
 <?php
+define('FORCE_SSL_ADMIN', true);
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+    $_SERVER['HTTPS']='on';
+
 /**
  * The base configuration for WordPress
  *
